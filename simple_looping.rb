95b21eb4b9e66c239c7c09d05bc5d56d87a8f975
Francis Mendez <fmendez884@gmail.com>
2019-04-08 23:46:17 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,13 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  int = 0
+  loop do
+    puts phrase
+      int += 1
+    break if int == number_of_times
+  end
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -8,7 +15,6 @@ def loop_iterator(number_of_times)
   #   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
-  phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
 
@@ -16,6 +22,9 @@ def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  7.times do
+    puts phrase
+  end
 end
 
 def while_iterator(number_of_times)
@@ -23,13 +32,22 @@ def while_iterator(number_of_times)
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  i = 0 
+  while i < number_of_times
+    puts phrase
+    i += 1
+  end
 end
 
 def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  i = 0 
+    until i == number_of_times
+      puts phrase
+      i += 1 
+    end
 end
 
 def for_iterator(number_of_times)
@@ -37,6 +55,9 @@ def for_iterator(number_of_times)
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  i = (1..number_of_times)
+  for yes in i
+    puts phrase
+  end
 end
 
