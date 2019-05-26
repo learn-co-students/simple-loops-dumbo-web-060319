f29044979347b8c9c983c650b5f24f73b37e0568
Olga Rosas <olga.rosas@gmail.com>
2019-05-26 11:46:13 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,7 +37,7 @@ end
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   count = 1..7
-  for count in number_of_times
+  for times in number_of_times
     puts phrase
   end
 end
